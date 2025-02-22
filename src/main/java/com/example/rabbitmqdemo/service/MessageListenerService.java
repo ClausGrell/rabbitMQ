c5d6d33df68688677945bdc89c24911da586a6b5
Claus <claus@grell.dk>
2025-02-22 08:06:46 +0100

    Init

--- src/main/java/com/example/rabbitmqdemo/service/MessageListenerService.java
+++ src/main/java/com/example/rabbitmqdemo/service/MessageListenerService.java
@@ -25,12 +25,15 @@ public class MessageListenerService {
     public void receiveMessageh(Message message) {
         try {
             String body = new String(message.getBody());
+            System.out.println("body: " + body);
 
             // Access the headers
             MessageProperties messageProperties = message.getMessageProperties();
             Map<String, Object> headers = messageProperties.getHeaders();
 
             ObjectMapper objectMapper = new ObjectMapper();
+//            Map jsonMap = objectMapper.readValue(body, Map.class);
+
             JsonNode event = objectMapper.readTree(body);
 
             // Example: Extract some fields from the event message
@@ -53,3 +56,46 @@ public class MessageListenerService {
 
 
 }
+
+
+/*{
+  "event": "s3:ObjectCreated:Put",
+  "bucket": {
+    "name": "example-bucket"
+  },
+  "object": {
+    "key": "path/to/object/file.txt",
+    "size": 2048,
+    "etag": "d41d8cd98f00b204e9800998ecf8427e",
+    "content-type": "text/plain"
+  },
+  "timestamp": "2025-02-22T12:00:00Z",
+  "user_metadata": {
+    "author": "Jane Doe",
+    "tags": ["document", "file"]
+  },
+  "aws_region": "us-west-2",
+  "event_source": "aws:s3",
+  "event_source_arn": "arn:aws:s3:::example-bucket",
+  "request_id": "abcd1234efgh5678",
+  "source_ip_address": "192.168.1.100"
+}
+*/
+
+/*
+{
+  "event": "object_uploaded",
+  "bucket": "example-bucket",
+  "object_key": "path/to/object/file.txt",
+  "object_size": 2048,
+  "content_type": "text/plain",
+  "timestamp": "2025-02-22T12:00:00Z",
+  "etag": "d41d8cd98f00b204e9800998ecf8427e",
+  "storage_class": "STANDARD",
+  "user_metadata": {
+    "author": "John Doe",
+    "tags": ["documentation", "file"]
+  },
+  "originating_system": "ceph"
+}
+ */
